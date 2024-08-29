e821395efae815340ad0027bc04c511627aa1168
moazkassim <moazkassim100@gmail.com>
2024-08-29 16:24:06 +0100

    solve image issues

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -7,42 +7,42 @@ export default function Projects() {
       title: "Floor Plan Design",
       description:
         "Our project centers around the development of an innovative solution for automated floor plan generation using AI. Traditional floor plan design processes can be time-consuming and resource-intensivedasd",
-      src: "public/projects/floorPlan.jpg",
+      src: "//public/projects/floorPlan.jpg",
     },
     {
       title: "ATM System",
       description:
         "An Automated Teller Machine (ATM) system that provides banking services to customers through a machine, allowing them to withdraw cash, check account balances, transfer funds, and more. Below are some core components and processes typically involved in an ATM system",
 
-      src: "public/projects/atm.JPG",
+      src: "/public/projects/atm.JPG",
     },
     {
       title: "C Mosfet ALU",
       description:
         " Design and implement a basic ALU using CMOS transistors to perform arithmetic and logical operations. Interface the ALU with an ESP32 for control, and use a webcam to stream a live demo of the circuit operation.",
 
-      src: "public/projects/alu.JPG",
+      src: "/public/projects/alu.JPG",
     },
     {
       title: "To-Do List",
       description:
         "A To-Do List project using HTML, CSS, and JavaScript is a simple web application that allows users to add, manage, and delete tasks. It can include basic functionality",
 
-      src: "public/projects/toDoList.JPG",
+      src: "/public/projects/toDoList.JPG",
     },
     {
       title: "E-Commerce",
       description:
         "An e-commerce website project involves creating an online platform where products or services are bought and sold over the internet. providing a user-friendly interface for browsing products, adding them to a shopping cart .",
 
-      src: "public/projects/ecomerce.JPG",
+      src: "/public/projects/ecomerce.JPG",
     },
     {
       title: "React Note",
       description:
         "A React project for online notes using React Markdown (react-markdown) is a web application that allows users to create, edit, and manage their notes online, with the capability to format these notes using Markdown syntax.",
 
-      src: "public/projects/note.JPG",
+      src: "/public/projects/note.JPG",
     },
   ];
   return (
